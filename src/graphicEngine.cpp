123ad469cb0ea60c7ea4b23937d7fcf7c0fa648a
spotter1006 <you@example.comscott@taftinstruments.com>
2024-03-01 17:11:17 -0500

    checkpoint

--- src/graphicEngine.cpp
+++ src/graphicEngine.cpp
@@ -1,13 +1,17 @@
 #include "graphicEngine.hpp"
 #include "frame.hpp"
+#include <sys/time.h>
+#include <unistd.h>
+#include <signal.h>
+
 
 using namespace std;
 
 GraphicEngine::GraphicEngine(int frames, int bars, int pixels){
-    m_frames = vector<Frame>(frames, Frame(bars, pixels));
+    m_fKeepRunning = true;
 }
 GraphicEngine::~GraphicEngine(){
-    m_frames.clear();
+
 }
 void GraphicEngine::addElement(GraphicElement element){
     m_elements.push_back(element);
@@ -16,4 +20,27 @@ void GraphicEngine::render(int step, Frame &frame){
     for(GraphicElement el : m_elements){
         el.render(step, frame);
     }
+}
+atomic_flag fWait;
+void GraphicEngine::start(int intervalUs){
+    signal(SIGALRM, [](int signo){fWait.clear();});   
+    itimerval timer;
+    timer.it_interval.tv_usec = intervalUs;      
+    timer.it_interval.tv_sec = 0;
+    timer.it_value.tv_usec = intervalUs;
+    timer.it_value.tv_sec = 0;
+    setitimer(ITIMER_REAL, &timer, NULL);
+
+    m_thread = thread([](GraphicEngine *pGraphicEngine){
+        while(pGraphicEngine->isKeepRunning()){
+            while(fWait.test_and_set()){usleep(2);}
+            // render for tick
+            // set frame in target
+        }
+        
+    },this);
+}
+void GraphicEngine::stop(){
+    m_fKeepRunning = false;
+    m_thread.join();
 }
--- src/graphicEngine.hpp
+++ src/graphicEngine.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "graphicElement.hpp"
 #include "frame.hpp"
+#include <thread>
 
 using namespace std;
 
@@ -10,12 +11,15 @@ class GraphicEngine{
     public:
         GraphicEngine(int nFrames, int bars, int pixels);
         ~GraphicEngine();
-
+        void start(int intervalUs);
+        void stop();
         void addElement(GraphicElement element);
-        void render(int step, Frame& frame);       
+        void render(int step, Frame& frame); 
+        inline bool isKeepRunning(){return m_fKeepRunning;}      
     private:
-        vector<GraphicElement> m_elements;      // Inputs to engine
-        vector<Frame> m_frames;                 // Output of engine
+        vector<GraphicElement> m_elements;      
+        bool m_fKeepRunning;
+        thread m_thread;
 };
 
 #endif
