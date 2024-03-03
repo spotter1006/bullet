dc320591f6b09789e1808a13a229211d44218a2e
spotter1006 <you@example.comscott@taftinstruments.com>
2024-03-02 19:35:42 -0500

    checkpoint

--- src/graphicEngine.cpp
+++ src/graphicEngine.cpp
@@ -6,21 +6,13 @@
 
 using namespace std;
 
-GraphicEngine::GraphicEngine(int frames, int bars, int pixels){
-    m_fKeepRunning = true;
-    m_pFrame = NULL;
-}
-GraphicEngine::~GraphicEngine(){
-
-}
-
 void GraphicEngine::start(){
     m_fKeepRunning = true;
     m_threads.emplace_back(thread([](GraphicEngine *pGraphicEngine){
         int timeInterval = 0;
         while(pGraphicEngine->isKeepRunning()){
             auto wake = chrono::steady_clock::now() + chrono::milliseconds(AMIMATION_INTERVAL_MS); 
-            // Paint all patterns to the frame
+            // All patterns paint to display
             // TODO: put the frame in the polar display
             this_thread::sleep_until(wake);
         }  
--- src/graphicEngine.hpp
+++ src/graphicEngine.hpp
@@ -3,20 +3,24 @@
 #include <vector>
 #include "frame.hpp"
 #include <thread>
+#include "pattern.hpp"
 
 using namespace std;
 
 class GraphicEngine{
     public:
-        GraphicEngine(int nFrames, int bars, int pixels);
-        ~GraphicEngine();
+        GraphicEngine(int nFrames, int bars, int pixels) : m_patterns(){
+            m_fKeepRunning = true;
+            m_pFrame = NULL;
+        };
+
         void start();
         void stop();
 
         inline bool isKeepRunning(){return m_fKeepRunning;}   
         inline void setFrame(Frame *pFrame){m_pFrame = pFrame;}
     private:
-  
+        vector<Pattern> m_patterns;
         Frame *m_pFrame; 
         bool m_fKeepRunning;
         vector<thread> m_threads;
