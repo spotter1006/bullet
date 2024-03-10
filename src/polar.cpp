08b9a7b898a289c3f8607555a98aa643dff1e89a
spotter1006 <you@example.comscott@taftinstruments.com>
2024-03-10 08:35:48 -0400

    colors and intensities

--- src/polar.cpp
+++ src/polar.cpp
@@ -10,23 +10,14 @@ using namespace std;
 
 atomic_flag fWaitForTick;   
 
-const uint8_t gamma[] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
-    2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
-    5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
-    10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-    17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-    25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-    37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-    51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-    69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-    90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
-    115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
-    144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
-    177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
-    215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+const ws2811_led_t gradient[] = {                       // https://colordesigner.io/gradient-generator
+    0x00fe0101, 0x00fe2302, 0x00fe4503, 0x00fe6704,     // green to red
+    0x00fe8805, 0x00feaa06, 0x00fecb07, 0x00feec08,
+    0x00f1ff08, 0x00d1ff09, 0x00b1ff0a, 0x0091ff0b,
+    0x0071ff0c, 0x0052ff0d, 0x0033ff0e, 0x0014ff0f
+};
+
 
 
 Polar::Polar(int left, int right, int radius):
@@ -35,7 +26,7 @@ Polar::Polar(int left, int right, int radius):
     m_nInterval(100),
     m_nAngle(0),
     m_chaser(radius),
-    m_intensities({0, 2, 8, 32, 128, 0, 2, 8, 32, 128}),
+    m_intensities({0, 0, 3, 7, 27, 35, 58, 89, 129, 180}),      // Gamma corrected ramp
     m_colors(radius, BLUE)
     {
         signal(SIGALRM, [](int signo){fWaitForTick.clear();});   
@@ -76,9 +67,8 @@ void Polar::stop(){
 }
 
 ws2811_led_t Polar::redToGreen(int val){
-    int green = val + 128;
-    int red = 128 - val;
-    return gamma[red]  << 16 | gamma[green] << 8;
+    return gradient[(val + 128) / 16];
+   
 }
 
 void Polar::setHue(int hue){
