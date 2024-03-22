675b5ace4e1dab841d673d8fb2a52ef476557d53
spotter1006 <you@example.comscott@taftinstruments.com>
2024-03-22 15:20:52 -0400

    adding histogram unit tests

--- src/test/testpolar.cpp
+++ src/test/testpolar.cpp
@@ -1,8 +1,35 @@
 #include "../polar.hpp"
-int main(int argc, char* argv[] ){
-    Polar* pPolar = new Polar(-MOTOR_SWEEP_STEPS/2, MOTOR_SWEEP_STEPS/2, LED_STRING_PIXELS);
+#include <iostream>
+#include<sstream>
 
-    pPolar->incrementHeading(-2.25);
-    int result = pPolar->getHeadingVariance(HEADING_BUCKETS/2, 22);
+void evaluate(int expected, int result){
+        ostringstream error;
+    if(result == expected){
+        cout << "pass" << endl;
+    }else{
+        error << "fail. Expected " << expected << ", got " << result;
+        throw(error.str());
+    }
+}
+
+int main(int argc, char* argv[] ){
+
+    Polar* pPolar = new Polar(-MOTOR_SWEEP_STEPS/2, MOTOR_SWEEP_STEPS/2, LED_STRING_PIXELS);
+    try{
+        
+        cout << "Single value variance test ... " ;
+        pPolar->incrementHeading(-2.25);
+        evaluate(810, pPolar->getHeadingVariance(HEADING_BUCKETS/2, 22));
+
+        cout << "Symetric values histogram test ... ";
+        pPolar->incrementHeading(2.25);
+        evaluate(800, pPolar->getHeadingVariance(HEADING_BUCKETS/2, 22));
+
+
+
+    }catch(string error){
+        cout << error << endl;
+    }
 
 }
+
